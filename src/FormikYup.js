bacc71de233a874c3d7f6b42807fc23fc863e188
unknown <quang.luong2@executionlab.asia>
2022-08-04 10:31:31 +0700

    Demo Form by Formik, Form, Field & Yup

--- src/FormikYup.js
+++ src/FormikYup.js
@@ -1,74 +1,95 @@
-import { useFormik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from 'yup'
 
 function FormikYup() {
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-        },
-        validationSchema: Yup.object({
-            name: Yup.string().required('The field is required'),
-            email: Yup.string().email('Error Syntax').required('The field is required'),
-            password: Yup.string().min(5, 'Enter at least 5 characters !').required('The field is required'),
-            confirmPassword: Yup.string().oneOf([Yup.ref('password')], 'Password do not match !').required('The field is required'),
-        }),
-        onSubmit: value => {
-            console.log(value);
-        }
+    
+    const initialValues = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '' 
+    }
+    const validate = Yup.object({
+        name: 
+            Yup
+            .string()
+            .required('The field is required'),
+        email: 
+            Yup
+            .string()
+            .email('Error Syntax !')
+            .required('The field is required'),
+        password: 
+            Yup
+            .string()
+            .min(5, 'Enter at least 5 characters !')
+            .required('The field is required'),
+        confirmPassword: 
+            Yup
+            .string()
+            .oneOf([Yup.ref('password')], 'Password do not match !')
+            .required('The field is required')
     })
+    const submitForm = value => {
+        console.log(value);
+    }
+
   return (
-    <div className='form'>
-        <form onSubmit={formik.handleSubmit}>
-            <div className='form__group'>
-                <label>Name:</label>
-                <input 
-                    type='text' 
-                    name='name' 
-                    autoComplete="off"
-                    value={formik.values.name}
-                    onChange={formik.handleChange}
-                />
-                {formik.errors.name && formik.touched.name ? <span className='form__error'>{formik.errors.name}</span> : null}
-            </div>
-            <div className='form__group'>
-                <label>Email:</label>
-                <input 
-                    type='text' 
-                    name='email' 
-                    autoComplete="off"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                />
-                {formik.errors.email && formik.touched.email ? <span className='form__error'>{formik.errors.email}</span> : null}
-            </div>
-            <div className='form__group'>
-                <label>Password:</label>
-                <input 
-                    type='password' 
-                    name='password' 
-                    autoComplete="off"
-                    value={formik.values.password}
-                    onChange={formik.handleChange}
-                />
-                {formik.errors.password && formik.touched.password ? <span className='form__error'>{formik.errors.password}</span> : null}
-            </div>
-            <div className='form__group'>
-                <label>Confirm Password:</label>
-                <input 
-                    type='password' 
-                    name='confirmPassword' 
-                    autoComplete="off"
-                    value={formik.values.confirmPassword}
-                    onChange={formik.handleChange}
-                />
-                {formik.errors.confirmPassword && formik.touched.confirmPassword ? <span className='form__error'>{formik.errors.confirmPassword}</span> : null}
-            </div>
-            <button type='submit' className='form__submit'>Submit</button>
-        </form>
-    </div>
+    <Formik 
+        initialValues={initialValues} 
+        validationSchema={validate} 
+        onSubmit={submitForm}
+    >
+        {
+            ({errors, touched}) => (
+                <div className='form'>
+                    <Form>
+                        <div className='form__group'>
+                            <label>Name:</label>
+                            <Field 
+                                type='text' 
+                                name='name' 
+                                autoComplete="off"
+                                className={errors.name && touched.name ? 'form__input--error' : ''}
+                            />
+                            <ErrorMessage name='name' component='span' className='form__error' />
+                        </div>
+                        <div className='form__group'>
+                            <label>Email:</label>
+                            <Field 
+                                type='text' 
+                                name='email' 
+                                autoComplete="off"
+                                className={errors.email && touched.email ? 'form__input--error' : ''}
+                            />
+                            <ErrorMessage name='email' component='span' className='form__error' />
+                        </div>
+                        <div className='form__group'>
+                            <label>Password:</label>
+                            <Field 
+                                type='password' 
+                                name='password' 
+                                autoComplete="off"
+                                className={errors.password && touched.password ? 'form__input--error' : ''}
+                            />
+                            <ErrorMessage name='password' component='span' className='form__error' />
+                        </div>
+                        <div className='form__group'>
+                            <label>Confirm Password:</label>
+                            <Field 
+                                type='password' 
+                                name='confirmPassword' 
+                                autoComplete="off"
+                                className={errors.confirmPassword && touched.confirmPassword ? 'form__input--error' : ''}
+                            />
+                            <ErrorMessage name='confirmPassword' component='span' className='form__error' />
+                        </div>
+                        <button type='submit' className='form__submit'>Submit</button>
+                    </Form>
+                </div>
+            )
+        }
+    </Formik>
   );
 }
 
